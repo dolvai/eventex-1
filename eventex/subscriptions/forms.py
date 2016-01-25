4eb1e907155753febaf892a7ff2c0799a55339b9
andersonnunes <anderson.nunes1988@gmail.com>
2016-01-25 10:44:28 -0200

    Simplifica com ModelForm.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,20 +1,12 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription
 
 
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números.', 'digits')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números.', 'length')
-
-
-class SubscriptionForm(forms.Form):
-    name = forms.CharField(label="Nome")
-    cpf = forms.CharField(label="CPF", validators=[validate_cpf])
-    email = forms.EmailField(label="Email", required=False)
-    phone = forms.CharField(label="Telefone", required=False)
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
 
     def clean_name(self):
         name = self.cleaned_data['name']
